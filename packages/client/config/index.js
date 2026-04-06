78d3cfee45f5d6ba543687751d7197058e3192a6
King Hong <45602006+a2064777923@users.noreply.github.com>
2026-04-06 10:56:41 +0800

    Add packages/client/config/index.js

---
+++ packages/client/config/index.js
@@ -0,0 +1,67 @@
+const config = {
+  projectName: 'trip-of-macau',
+  date: '2026-4-1',
+  designWidth: 750,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+  },
+  sourceRoot: 'src',
+  outputRoot: 'dist',
+  plugins: [],
+  defineConstants: {},
+  copy: {
+    patterns: [],
+    options: {},
+  },
+  framework: 'react',
+  compiler: 'webpack5',
+  cache: {
+    enable: false,
+  },
+  mini: {
+    compile: {
+      exclude: [],
+    },
+    postcss: {
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 1024,
+        },
+      },
+      cssModules: {
+        enable: true,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+  h5: {
+    publicPath: '/',
+    staticDirectory: 'static',
+    output: {},
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {},
+      },
+      cssModules: {
+        enable: true,
+        config: {
+          namingPattern: 'module',
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
